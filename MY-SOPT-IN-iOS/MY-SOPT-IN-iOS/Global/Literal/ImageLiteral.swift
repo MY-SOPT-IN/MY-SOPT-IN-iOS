f01bf5240e6e4118e6824a7a68ab2b2bb976482d
Daye Kim <dykim315@naver.com>
2023-05-16 02:43:36 +0900

    [Setting] #1 - 이미지, 아이콘 세팅

--- MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Global/Literal/ImageLiteral.swift
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Global/Literal/ImageLiteral.swift
@@ -5,4 +5,47 @@
 //  Created by 김다예 on 2023/05/16.
 //
 
-import Foundation
+import UIKit
+
+struct ImageLiterals {
+    
+    struct TabBar {
+        static let home = UIImage(named: "tabbar_ic_home.png")
+        static let like = UIImage(named: "tabbar_ic_like.png")
+        static let achieve = UIImage(named: "tabbar_ic_achieve.png")
+        static let social = UIImage(named: "tabbar_ic_social.png")
+        static let myProfile = UIImage(named: "tabbar_ic_myProfile.png")
+    }
+    
+    struct TopBar {
+        static let routine = UIImage(named: "display=routine.png")
+        static let week = UIImage(named: "display=day.png")
+        static let recent = UIImage(named: "recent.png")
+    }
+    
+    struct RecallProperty {
+        static let defaultRecall = UIImage(named: "Property 1=Default.png")
+        static let variantRecall = UIImage(named: "Property 1=Variant2.png")
+    }
+    
+    struct Icon {
+        static let add_ic_bar_graph = UIImage(named: "add_ic_bar_graph.png")
+        static let add_ic_graph = UIImage(named: "add_ic_graph.png")
+        static let add_ic_checkbox = UIImage(named: "add_ic_checkbox.png")
+        static let add_ic_bell = UIImage(named: "add_ic_bell.png")
+        static let add_ic_lock = UIImage(named: "add_ic_lock.png")
+        static let add_ic_meditation = UIImage(named: "add_ic_meditation.png")
+        static let add_ic_milk = UII mage(named: "add_ic_milk.png")
+        static let add_ic_newspaper = UIImage(named: "add_ic_newspaper.png")
+        static let add_ic_dinosaur = UIImage(named: "add_ic_dinosaur.png")
+        static let add_ic_edit = UIImage(named: "add_ic_edit.png")
+        static let add_ic_leaf = UIImage(named: "add_ic_leaf.png")
+        static let add_ic_x = UIImage(named: "add_ic_x.png")
+        static let add_ic_delete = UIImage(named: "add_ic_delete.png")
+        static let add_ic_check = UIImage(named: "add_ic_check.png")
+        static let add_ic_copy = UIImage(named: "add_ic_copy.png")
+        static let add_ic_arrow = UIImage(named: "add_ic_arrow.png")
+        static let add_ic_dots_menu = UIImage(named: "add_ic_dots_menu.png")
+        static let add_ic_bar = UIImage(named: "add_ic_bar.png")
+    }
+}
