dea429fe7d3c9e1056fa48265e9c4768b2006b3d
Daye Kim <dykim315@naver.com>
2023-05-18 18:36:51 +0900

    [Feat] #12 - 메인 페이지 페이징 구현
    indicator 구현 필요

--- MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageTopBarView.swift
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageTopBarView.swift
@@ -10,9 +10,15 @@ import UIKit
 import SnapKit
 import Then
 
-class MainPageTopBarView: UIView {
+final class MainPageTopBarView: UIView {
 
     // MARK: - Properties
+    
+    let stackView = UIStackView()
+    let routineLabel = UILabel()
+    let recallLabel = UILabel()
+    let indicator = UIView()
+    private let dividingLine = UIView()
 
     // MARK: - View Life Cycle
 
@@ -23,17 +29,68 @@ class MainPageTopBarView: UIView {
         setLayout()
     }
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Methods
     
     func setStyle() {
-        self.backgroundColor = .Mono.white
+        
+        stackView.do {
+            $0.backgroundColor = .Mono.white
+            $0.axis = .horizontal
+            $0.distribution = .fillEqually
+        }
+        
+        routineLabel.do {
+            $0.text = "루틴"
+            $0.font = .title1Font()
+            $0.textColor = .Gray.gray_700
+            $0.textAlignment = .center
+        }
+        
+        recallLabel.do {
+            $0.text = "회고"
+            $0.font = .title1Font()
+            $0.textColor = .Gray.gray_700
+            $0.textAlignment = .center
+        }
+        
+        indicator.do {
+            $0.backgroundColor = .Primary.primary_900
+        }
+        
+        dividingLine.do {
+            $0.backgroundColor = .Gray.gray_200
+        }
     }
-    func setLayout() {}
+    func setLayout() {
+        
+        self.addSubviews(stackView,
+                         dividingLine)
+
+        stackView.addArrangedSubviews(routineLabel,
+                                 recallLabel)
+        stackView.addSubview(indicator)
+        
+        stackView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(dividingLine.snp.top)
+        }
+    
+        indicator.snp.makeConstraints {
+            $0.height.equalTo(3)
+            $0.width.equalTo(114)
+            $0.bottom.equalToSuperview()
+            $0.centerX.equalTo(routineLabel.snp.centerX)
+        }
+        
+        dividingLine.snp.makeConstraints {
+            $0.height.equalTo(1.5)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+    }
 
     // MARK: - @objc Function
 
--- MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageViewController.swift
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageViewController.swift
@@ -14,7 +14,13 @@ class MainPageViewController: UIViewController {
 
     // MARK: - Properties
     
-    private var topBar = MainPageTopBarView()
+    private let topBar = MainPageTopBarView()
+    private let pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    
+    lazy var routineViewController = MainPageRoutineViewController()
+    lazy var recallViewController = MainPageRecallViewController()
+    
+    lazy var viewControllers = [routineViewController, recallViewController]
 
     // MARK: - View Life Cycle
     
@@ -27,23 +33,62 @@ class MainPageViewController: UIViewController {
     
     // MARK: - Methods
     
-    func setStyle() {
+    private func setStyle() {
         view.backgroundColor = .Mono.white
         topBar.backgroundColor = .Gray.gray_50
+        
+        setPageController()
     }
     
-    func setLayout() {
-        view.addSubviews(topBar)
+    private func setLayout() {
+        view.addSubviews(topBar, pageController.view)
+        addChild(pageController)
+        pageController.didMove(toParent: self)
         
         topBar.snp.makeConstraints {
             $0.height.equalTo(44)
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
         }
+        
+        pageController.view.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalToSuperview()
+            $0.top.equalTo(topBar.snp.bottom)
+        }
+    }
+    
+    private func setPageController() {
+        pageController.do {
+            $0.delegate = self
+            $0.dataSource = self
+        }
+        
+        if let firstVC = viewControllers.first {
+            pageController.setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
     }
 
     // MARK: - @objc Function
     
     // MARK: - Network
+
+}
+
+extension MainPageViewController: UIPageViewControllerDelegate {}
+extension MainPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = viewControllers.firstIndex(of: viewController ) else { return nil }
+        let previousIndex = index - 1
+        if previousIndex < 0 { return nil }
+        return viewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = viewControllers.firstIndex(of: viewController ) else { return nil }
+        let nextIndex = index + 1
+        if nextIndex > viewControllers.count - 1 { return nil }
+        return viewControllers[nextIndex]
+    }
 
 }
