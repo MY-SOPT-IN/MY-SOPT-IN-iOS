8ed4ee91b9335d1d7dd72a66fb38b1d00cd1ec53
Daye Kim <dykim315@naver.com>
2023-05-23 17:54:27 +0900

    [Feat] #12 - 메인 페이지 회고 날짜 선택 뷰 추가

--- MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageRecallViewController.swift
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/VC/MainPageRecallViewController.swift
@@ -14,11 +14,22 @@ final class MainPageRecallViewController: UIViewController {
 
     // MARK: - Properties
 
+    private let headerView = MainPageRecallHeaderView()
+    
+    private var dateDummy: [[Dates]] = [Dates.getPreviousDateDummy(),
+                                        Dates.dummy(),
+                                        Dates.getNextDateDummy()]
+    
+    private var headerViewStartPoint: CGFloat = 0
+    
+    private var selectedDay = Dates.getToday()?.dateComponents
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        target()
         setStyle()
         setHierarchy()
         setLayout()
@@ -26,17 +37,126 @@ final class MainPageRecallViewController: UIViewController {
 
     // MARK: - Methods
     
+    private func target() {
+        
+        headerView.dateScrollView.delegate = self
+        headerView.dateScrollView.contentOffset.x = UIScreen.main.bounds.width
+        
+        [headerView.previousSelectDateCollectionView, headerView.currentSelectDateCollectionView, headerView.nextSelectDateCollectionView].forEach {
+            $0.dataSource = self
+            $0.delegate = self
+            $0.register(SelectDateCVC.self, forCellWithReuseIdentifier: SelectDateCVC.identifier)
+            $0.register(SelectDateHeaderFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SelectDateHeaderFooter.identifier)
+            $0.register(SelectDateHeaderFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: SelectDateHeaderFooter.identifier)
+        }
+    }
+    
     private func setStyle() {
         
         view.backgroundColor = .Gray.gray_50
     }
     
-    private func setHierarchy() { }
+    private func setHierarchy() {
+        
+        view.addSubviews(headerView)
+    }
     
-    private func setLayout() { }
+    private func setLayout() {
+        
+        headerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+    }
 
     // MARK: - @objc Function
     
     // MARK: - Network
 
+}
+
+extension MainPageRecallViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let index = headerView.dateCollectionViews.firstIndex(of: collectionView as! SelectDateCollectionView) {
+            selectedDay = dateDummy[index][indexPath.item].dateComponents
+            collectionView.reloadData()
+            headerView.dateLabel.text = dateDummy[index][indexPath.item].getDateString()
+        }
+    }
+}
+
+extension MainPageRecallViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let index = headerView.dateCollectionViews.firstIndex(of: collectionView as! SelectDateCollectionView) {
+            return dateDummy[index].count
+        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SelectDateCVC.identifier, for: indexPath) as? SelectDateCVC else { return UICollectionViewCell() }
+        if let index = headerView.dateCollectionViews.firstIndex(of: collectionView as! SelectDateCollectionView) {
+            if selectedDay == dateDummy[index][indexPath.item].dateComponents {
+                cell.configCell(date: dateDummy[index][indexPath.item], selected: true)
+            } else {
+                cell.configCell(date: dateDummy[index][indexPath.item], selected: false)
+            }
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SelectDateHeaderFooter.identifier, for: indexPath) as? SelectDateHeaderFooter else { return SelectDateHeaderFooter() }
+            return header
+        } else {
+            guard let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: SelectDateHeaderFooter.identifier, for: indexPath) as? SelectDateHeaderFooter else { return SelectDateHeaderFooter() }
+            return footer
+        }
+    }
+}
+
+extension MainPageRecallViewController: UIScrollViewDelegate {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        headerViewStartPoint = targetContentOffset.pointee.x
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        switch headerViewStartPoint {
+        case 0:
+            previousDates()
+        case UIScreen.main.bounds.width:
+            break
+        case UIScreen.main.bounds.width * 2:
+            nextDates()
+        default:
+            break
+        }
+    }
+    
+    private func previousDates() {
+        for i in [2, 1] {
+            dateDummy[i] = dateDummy[i - 1]
+        }
+        dateDummy[0] = Dates.getPreviousDateDummy(current: dateDummy[1])
+        reloadDateData()
+    }
+    
+    private func nextDates() {
+        for i in [0, 1] {
+            dateDummy[i] = dateDummy[i + 1]
+        }
+        dateDummy[2] = Dates.getNextDateDummy(current: dateDummy[1])
+        reloadDateData()
+    }
+    
+    private func reloadDateData() {
+        headerView.previousSelectDateCollectionView.reloadData()
+        headerView.currentSelectDateCollectionView.reloadData()
+        headerView.nextSelectDateCollectionView.reloadData()
+        
+        headerView.dateScrollView.contentOffset.x = UIScreen.main.bounds.width
+    }
 }
---
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Presentation/MainPage/Views/MainPageRecallHeaderView.swift
@@ -0,0 +1,122 @@
+//
+//  MainPageRecallHeaderView.swift
+//  MY-SOPT-IN-iOS
+//
+//  Created by 김다예 on 2023/05/23.
+//
+
+import UIKit
+
+class MainPageRecallHeaderView: UIView {
+
+    // MARK: - Properties
+
+    let dateScrollView = UIScrollView()
+    var previousSelectDateCollectionView = SelectDateCollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var currentSelectDateCollectionView = SelectDateCollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var nextSelectDateCollectionView = SelectDateCollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    lazy var dateCollectionViews = [previousSelectDateCollectionView,
+                                    currentSelectDateCollectionView,
+                                    nextSelectDateCollectionView]
+    
+    private var dateView = UIView()
+    private var achieveRecallBtn = UIButton()
+    var dateLabel = UILabel()
+    
+    private let screenWidth = UIScreen.main.bounds.width
+    
+    // MARK: - View Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+    
+    private func setStyle() {
+        
+        dateScrollView.do {
+            $0.isPagingEnabled = true
+            $0.showsHorizontalScrollIndicator = false
+            $0.contentSize.width = screenWidth * 3
+        }
+
+        dateLabel.do {
+            $0.text = Dates.getDateString(Dates.getToday()!)()
+            $0.font = .title2Font()
+            $0.textColor = .Gray.gray_800
+        }
+        
+        achieveRecallBtn.do {
+            $0.setTitle("회고 모아보기", for: .normal)
+            $0.setTitleColor(.Gray.gray_800, for: .normal)
+            $0.titleLabel?.font = .subtitleFont()
+
+        }
+    }
+    
+    private func setHierarchy() {
+        
+        self.addSubviews(dateScrollView,
+                                 dateView)
+        
+        dateScrollView.addSubviews(previousSelectDateCollectionView,
+                                   currentSelectDateCollectionView,
+                                   nextSelectDateCollectionView)
+        
+        dateView.addSubviews(dateLabel,
+                             achieveRecallBtn)
+    }
+    
+    private func setLayout() {
+        
+        dateScrollView.snp.makeConstraints {
+            $0.height.equalTo(85)
+            $0.top.leading.trailing.equalToSuperview()
+        }
+        
+        previousSelectDateCollectionView.snp.makeConstraints{
+            $0.width.equalTo(screenWidth)
+            $0.height.equalTo(85)
+            $0.top.bottom.leading.equalToSuperview()
+        }
+        
+        currentSelectDateCollectionView.snp.makeConstraints{
+            $0.width.equalTo(screenWidth)
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(previousSelectDateCollectionView.snp.trailing)
+        }
+        
+        nextSelectDateCollectionView.snp.makeConstraints{
+            $0.width.equalTo(screenWidth)
+            $0.top.bottom.trailing.equalToSuperview()
+            $0.leading.equalTo(currentSelectDateCollectionView.snp.trailing)
+        }
+                        
+        dateView.snp.makeConstraints {
+            $0.top.equalTo(dateScrollView.snp.bottom)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        achieveRecallBtn.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+    }
+}
+
+
+
