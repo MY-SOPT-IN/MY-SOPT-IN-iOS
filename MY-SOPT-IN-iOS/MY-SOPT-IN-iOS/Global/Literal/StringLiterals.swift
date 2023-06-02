49e2f613e7fb3fa1e8e629b577f544a036ecaf4a
Cheon, Seongwoo <cjs1399@naver.com>
2023-06-02 16:33:56 +0900

    [Chore] #33 - StringLiterals 추가

---
+++ MY-SOPT-IN-iOS/MY-SOPT-IN-iOS/Global/Literal/StringLiterals.swift
@@ -0,0 +1,18 @@
+//
+//  StringLiterals.swift
+//  MY-SOPT-IN-iOS
+//
+//  Created by 천성우 on 2023/06/02.
+//
+
+import UIKit
+ 
+struct StringLiterals {
+    
+    struct Recall {
+        static let todayRoutine = "오늘 루틴 어땠어요?"
+        static let bestPart = "오늘은 뭐가 가장 좋았어요?"
+        static let selfMessage = "나에게 하고 싶은 말을 적어봐요 :)"
+    }
+    
+}
